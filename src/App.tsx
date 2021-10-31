0f83118e1d0c53839d8f4533c1768073eb00ed12
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-11-01 00:58:45 +0200

    add cookie consent

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { RootStoreProvider } from './mst/rootStoreContext';
 import BikeStationUpdateProvider from './context/bikeStationUpdateProvider';
 import Search from './components/search';
 import Help from './components/help';
+import CookieConsent from "react-cookie-consent";
 
 const App: React.FC = () => {
   const [rootTree, setRootTree] = useState<RootStore|undefined>();
@@ -28,6 +29,13 @@ const App: React.FC = () => {
             <Route exact path='/station/:stationId' component={StationInfo} />
             <Route exact path='/help' component={Help} />
           </Switch>
+          <CookieConsent
+            buttonText='Ymmärrän'
+            location='bottom'
+            visible="byCookieValue"
+          >
+            Omat kaupunkifillarit käyttää keksejä paremman käyttökokemuksen luomiseen. Lisää tietoa evästeistä voit lukea täällä <a href='https://policies.google.com/technologies/cookies?hl=fi-FI'>Googlen evästekäytäntö</a>
+          </CookieConsent>
         </div>
       </BikeStationUpdateProvider>
     </RootStoreProvider>
